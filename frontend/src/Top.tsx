bb1b09f86cca3d20170bc246a58731932c57d63f
toyota <cist.b220.s.toyota@gmail.com>
2022-12-05 02:58:37 +0900

    トップページの見た目完成

---
+++ frontend/src/Top.tsx
@@ -0,0 +1,32 @@
+import type { Component } from "solid-js";
+import Header from "./components/header";
+import Box from "@suid/material/Box";
+import GameTable from "./components/gameTable";
+import Zunda from "./components/zunda";
+import Grid from "@suid/material/Grid";
+import Credit from "./components/credit";
+import InputText from "./components/Input";
+import Button from "@suid/material/Button";
+
+const Top: Component = () => {
+  return (
+    <div
+      style={{ height: "100vh", "background-color": "#D9D9D9", width: "100%" }}
+    >
+      <Header />
+      <Grid container height="auto" bgcolor="#D9D9D9">
+        <GameTable />
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+        >
+          <Button variant="contained">ゲストユーザログイン</Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Top;
