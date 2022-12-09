0966d7a8a09166aa0eb8602ff136eec12c39ad92
toyota <cist.b220.s.toyota@gmail.com>
2022-12-09 22:40:36 +0900

    Start画面の見た目完成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,24 +9,26 @@ import InputText from "./components/Input";
 
 const App: Component = () => {
   return (
-    <div style={{height: "100vh", "background-color": "#D9D9D9", width: "100%"}}>
+    <div
+      style={{ height: "100vh", "background-color": "#D9D9D9", width: "100%" }}
+    >
       <Header />
       <Grid container height="auto" bgcolor="#D9D9D9">
-        <Grid container >
+        <Grid container>
           <Grid item height="100%" xs={6} p={1}>
             <GameTable />
           </Grid>
           <Grid container xs={6} p={1}>
             <Grid item xs={12} bgcolor="#9ccc65" borderRadius={2}>
-              <Zunda /> 
+              <Zunda />
             </Grid>
-            <Grid item xs={12} borderRadius={2} >
+            <Grid item xs={12} borderRadius={2}>
               <Credit />
             </Grid>
           </Grid>
         </Grid>
 
-        <Grid item borderRadius={2} bgcolor="white" margin={1} xs={12} >
+        <Grid item borderRadius={2} bgcolor="white" margin={1} xs={12}>
           <InputText />
         </Grid>
       </Grid>
