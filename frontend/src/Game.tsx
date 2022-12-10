3d5f8c76310b73c0a2b79fa942fbfa2ce8c8f60a
toyota <cist.b220.s.toyota@gmail.com>
2022-12-10 14:29:41 +0900

    ルーティング設定完了

---
+++ frontend/src/Game.tsx
@@ -0,0 +1,39 @@
+import type { Component } from "solid-js";
+import Header from "./components/header";
+import Box from "@suid/material/Box";
+import GameTable from "./components/gameTable";
+import Zunda from "./components/zunda";
+import Grid from "@suid/material/Grid";
+import Credit from "./components/credit";
+import InputText from "./components/Input";
+
+const Game: Component = () => {
+  return (
+    <div
+      style={{ height: "100vh", "background-color": "#D9D9D9", width: "100%" }}
+    >
+      <Header />
+      <Grid container height="auto" bgcolor="#D9D9D9">
+        <Grid container>
+          <Grid item height="100%" xs={6} p={1}>
+            <GameTable />
+          </Grid>
+          <Grid container xs={6} p={1}>
+            <Grid item xs={12} bgcolor="#9ccc65" borderRadius={2}>
+              <Zunda />
+            </Grid>
+            <Grid item xs={12} borderRadius={2}>
+              <Credit />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item borderRadius={2} bgcolor="white" margin={1} xs={12}>
+          <InputText />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Game;
